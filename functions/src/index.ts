88bffa2d433111d272a53b36651784050c89d3a1
Miguel Ângelo <miguelangelordantascn@gmail.com>
2025-07-29 18:11:47 +0000

    Crie um novo arquivo em `functions/src/index.ts` e adicione o seguinte c

---
+++ functions/src/index.ts
@@ -0,0 +1,6 @@
+// Este arquivo agora apenas importa e exporta suas funções,
+// sem nenhuma lógica própria, resolvendo o erro de deploy.
+
+import { findDuplicateContacts } from './jobs/findDuplicates';
+
+export { findDuplicateContacts };
