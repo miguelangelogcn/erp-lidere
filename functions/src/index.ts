38eb3843b72bd54d237f776693af5b787ef0f447
Miguel Ângelo <miguelangelordantascn@gmail.com>
2025-07-29 18:04:31 +0000

    Delete o seguinte arquivo: `functions/src/index.ts`.

--- functions/src/index.ts
+++
@@ -1,8 +0,0 @@
-// Importe e re-exporte todas as suas Cloud Functions aqui.
-
-// Importa a nova função do arquivo de jobs.
-// Certifique-se de que o caminho './jobs/findDuplicates' está correto.
-import { findDuplicateContacts } from './jobs/findDuplicates';
-
-// Exporta a função para que o Firebase possa descobri-la.
-export { findDuplicateContacts };
