fa9bc4674eef0f3d099a6ff7319a01678a5cec6a
Miguel Ângelo <miguelangelordantascn@gmail.com>
2025-07-30 20:07:23 +0000

    Crie um novo arquivo de configuração para o SDK de Administrador do Fire

---
+++ src/lib/firebase/admin.ts
@@ -0,0 +1,20 @@
+import * as admin from 'firebase-admin';
+
+// Evita a re-inicialização do app no ambiente de desenvolvimento
+if (!admin.apps.length) {
+  try {
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      }),
+      databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
+    });
+  } catch (error: any) {
+    console.error('Firebase admin initialization error', error.stack);
+  }
+}
+
+export const adminDb = admin.firestore();
+export const adminAuth = admin.auth();
